700476ea240ce12a29fc657bf2bca6f34a1c8cf1
JoHyunho-clover <78728072+JoHyunho-clover@users.noreply.github.com>
2021-07-20 16:31:03 +0900

    practice1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Dice from './Dice';
+
+function App() {
+    return (
+        <div>
+        <Dice color="red" num={2} />
+        </div>
+    );
+}
+export default App; //다른파일에서 사용할 수 있도록.
